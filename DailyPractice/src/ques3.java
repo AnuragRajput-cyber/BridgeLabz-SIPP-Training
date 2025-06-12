e883c3d340112f8cddac315ada4e0ec321fae049
AnuragRajput-cyber <anuragrajput2603@gmail.com>
2025-06-12 12:01:26 +0530

    _

---
+++ DailyPractice/src/ques3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ques3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int Height = sc.nextInt();
+        float inches = Height/2.84F;
+        float foot = Height/30.48F;
+        System.out.println("Your Height in cm is "+Height+" while in feet is "+ foot+ "and inches is "+inches);
+    }
+}
