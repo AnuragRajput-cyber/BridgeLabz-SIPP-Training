0627d802f5cfb61f856d9cd1b4d0dd9ff7ed587f
AnuragRajput-cyber <anuragrajput2603@gmail.com>
2025-06-12 12:13:28 +0530

    -

---
+++ DailyPractice/DAY-1_Questions/src/ques4.java
@@ -0,0 +1,8 @@
+public class ques4 {
+    public static void main(String[] args) {
+        int fee=125000;
+        int discountPercentage=10;
+        int discount = 100/discountPercentage;
+        int paybaleFees=
+    }
+}
