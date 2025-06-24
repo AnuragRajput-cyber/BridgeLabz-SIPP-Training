4415d65e3d0864934c194f6b4bbe3192b02dce47
Anurag Rajput <anuragrajput1226@gmail.com>
2025-06-24 12:58:55 +0530

    Delete DailyPractice directory

--- DailyPractice/DAY-1_ProgrammingElements/src/VolumeOfEarth.java
+++
@@ -1,9 +0,0 @@
-public class VolumeOfEarth {
-    public static void main(String[] args) {
-        double radius = 6378.0;
-        double volumeKm = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
-        double volumeMiles = volumeKm * Math.pow(0.621371, 3); // Convert km³ to mi³
-        System.out.println("The volume of Earth in cubic kilometers is " + volumeKm +
-                " and in cubic miles is " + volumeMiles);
-    }
-}
