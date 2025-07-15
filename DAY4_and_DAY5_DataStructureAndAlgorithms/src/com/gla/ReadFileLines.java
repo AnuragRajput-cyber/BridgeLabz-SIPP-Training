9a5941f9ca3af87ca013b2d058c1490fcb0361f2
Anurag Rajput <anuragrajput1226@gmail.com>
2025-07-15 09:38:32 +0530

    [Anurag] Add Read File Line by Line problem from FileReader section

--- DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/ReadFileLines.java
+++ DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/ReadFileLines.java
@@ -1,5 +1,17 @@
 package com.gla;
 
-public class ReadFileLines {
+import java.io.*;
 
+public class ReadFileLines {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("sample.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
