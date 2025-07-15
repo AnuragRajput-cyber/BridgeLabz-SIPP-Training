389df756b7680bcaaf1ebf71ae84d5a1646809cc
Anurag Rajput <anuragrajput1226@gmail.com>
2025-07-15 09:41:52 +0530

    [Anurag] Add Find Peak Element problem from Binary Search section

--- DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/FindPeakElement.java
+++ DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/FindPeakElement.java
@@ -1,5 +1,23 @@
 package com.gla;
 
 public class FindPeakElement {
+    public static int findPeak(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
 
+    public static void main(String[] args) {
+        int[] nums = {1,3,4,5,3,1};
+        int peakIndex = findPeak(nums);
+        System.out.println("Peak element index: " + peakIndex);
+    }
 }
+
