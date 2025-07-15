f2674e7bf385c507cdf5695876bb74918a666c06
Anurag Rajput <anuragrajput1226@gmail.com>
2025-07-15 09:41:08 +0530

    [Anurag] Add Find Rotation Point problem from Binary Search section

--- DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/FindRotationPoint.java
+++ DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/FindRotationPoint.java
@@ -1,5 +1,23 @@
 package com.gla;
 
 public class FindRotationPoint {
+    public static int findRotation(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
 
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        int index = findRotation(nums);
+        System.out.println("Rotation point index: " + index);
+    }
 }
+
