9202045462fbe7664ad51b620b6b31016154068a
Anurag Rajput <anuragrajput1226@gmail.com>
2025-07-15 09:42:31 +0530

    [Anurag] Add Count Word Occurrences problem from FileReader section

--- DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/CountWordOccurrences.java
+++ DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/CountWordOccurrences.java
@@ -1,5 +1,26 @@
 package com.gla;
 
-public class CountWordOccurrences {
+import java.io.*;
 
+public class CountWordOccurrences {
+    public static void main(String[] args) {
+        String targetWord = "the";
+        int count = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("sample.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    if (word.equalsIgnoreCase(targetWord)) {
+                        count++;
+                    }
+                }
+            }
+            System.out.println("Occurrences of '" + targetWord + "': " + count);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
