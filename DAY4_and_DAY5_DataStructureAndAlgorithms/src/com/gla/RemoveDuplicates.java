844773a7de8235c12c3277527f4026dfe1bf9c27
Anurag Rajput <anuragrajput1226@gmail.com>
2025-07-15 09:38:04 +0530

    er [Anurag] Add Remove Duplicates problem from StringBuilder section

--- DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/RemoveDuplicates.java
+++ DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/RemoveDuplicates.java
@@ -1,5 +1,25 @@
 package com.gla;
 
-public class RemoveDuplicates {
+import java.util.HashSet;
 
+public class RemoveDuplicates {
+    public static String removeDuplicates(String input) {
+        StringBuilder sb = new StringBuilder();
+        HashSet<Character> seen = new HashSet<>();
+
+        for (char c : input.toCharArray()) {
+            if (!seen.contains(c)) {
+                seen.add(c);
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "programming";
+        String result = removeDuplicates(str);
+        System.out.println("Without duplicates: " + result);
+    }
 }
+
