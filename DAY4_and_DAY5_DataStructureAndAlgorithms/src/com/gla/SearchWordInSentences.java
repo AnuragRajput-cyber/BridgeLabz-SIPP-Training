a9823a3037c64119107058e85ce45e091e1cf7e2
Anurag Rajput <anuragrajput1226@gmail.com>
2025-07-15 09:24:14 +0530

    [Anurag] Add Search Specific Word in Sentences problem from Linear Search section

--- DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/SearchWordInSentences.java
+++ DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/SearchWordInSentences.java
@@ -1,5 +1,23 @@
 package com.gla;
 
 public class SearchWordInSentences {
+    public static String search(String[] sentences, String word) {
+        for (String s : sentences) {
+            if (s.contains(word)) {
+                return s;
+            }
+        }
+        return "Not Found";
+    }
 
+    public static void main(String[] args) {
+        String[] sentences = {
+            "Java is powerful.",
+            "Python is easy.",
+            "C++ is fast."
+        };
+        String result = search(sentences, "easy");
+        System.out.println("Result: " + result);
+    }
 }
+
