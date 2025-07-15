42fa88f12a29a3eb1658d95f5c4ab09c73f68bac
Anurag Rajput <anuragrajput1226@gmail.com>
2025-07-15 09:40:42 +0530

    [Anurag] Add Find First Negative Number problem from Linear Search section

--- DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/FirstNegative.java
+++ DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/FirstNegative.java
@@ -1,5 +1,19 @@
 package com.gla;
 
 public class FirstNegative {
+    public static int findFirstNegative(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
+    public static void main(String[] args) {
+        int[] nums = {3, 4, -2, 7};
+        int index = findFirstNegative(nums);
+        System.out.println("First negative index: " + index);
+    }
 }
+
