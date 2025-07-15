9f8d2fe7dc636fd4a84a1e33c4d27294c3d546b8
Anurag Rajput <anuragrajput1226@gmail.com>
2025-07-15 09:43:06 +0530

    [Anurag] Add Concatenate Strings problem from StringBuffer section

--- DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/ConcatenateStrings.java
+++ DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/ConcatenateStrings.java
@@ -1,5 +1,18 @@
 package com.gla;
 
 public class ConcatenateStrings {
+    public static String concatenate(String[] strings) {
+        StringBuffer sb = new StringBuffer();
+        for (String s : strings) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
 
+    public static void main(String[] args) {
+        String[] arr = {"Hello", " ", "World", "!"};
+        String result = concatenate(arr);
+        System.out.println("Concatenated: " + result);
+    }
 }
+
