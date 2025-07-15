e29aeae2395d0dafab725bc53b2d2573c505631a
Anurag Rajput <anuragrajput1226@gmail.com>
2025-07-15 09:36:48 +0530

    [Anurag] Add Search Target in 2D Sorted Matrix problem from Binary Search section

--- DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/Search2DMatrix.java
+++ DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/Search2DMatrix.java
@@ -1,5 +1,29 @@
 package com.gla;
 
 public class Search2DMatrix {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length, cols = matrix[0].length;
+        int left = 0, right = rows * cols - 1;
 
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midElement = matrix[mid / cols][mid % cols];
+
+            if (midElement == target) return true;
+            if (midElement < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1,3,5},
+            {7,9,11},
+            {13,15,17}
+        };
+        boolean found = searchMatrix(matrix, 9);
+        System.out.println("Target found: " + found);
+    }
 }
+
