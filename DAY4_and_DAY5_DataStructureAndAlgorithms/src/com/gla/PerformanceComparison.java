11ef6211dc947b1d7ba4cfd107fa761ded217f5a
Anurag Rajput <anuragrajput1226@gmail.com>
2025-07-15 09:39:52 +0530

    [Anurag] Add Compare Performance problem from StringBuffer section

--- DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/PerformanceComparison.java
+++ DAY4_and_DAY5_DataStructureAndAlgorithms/src/com/gla/PerformanceComparison.java
@@ -1,5 +1,24 @@
 package com.gla;
 
 public class PerformanceComparison {
+    public static void main(String[] args) {
+        int n = 1_000_000;
 
+        long startBuffer = System.nanoTime();
+        StringBuffer sbf = new StringBuffer();
+        for (int i = 0; i < n; i++) {
+            sbf.append("hello");
+        }
+        long endBuffer = System.nanoTime();
+        System.out.println("StringBuffer time: " + (endBuffer - startBuffer) / 1_000_000 + " ms");
+
+        long startBuilder = System.nanoTime();
+        StringBuilder sbd = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sbd.append("hello");
+        }
+        long endBuilder = System.nanoTime();
+        System.out.println("StringBuilder time: " + (endBuilder - startBuilder) / 1_000_000 + " ms");
+    }
 }
+
