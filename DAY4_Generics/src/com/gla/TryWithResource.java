8a6e38c931c436f19d755dc8bf6ec66cad58efda
AnuragRajput-cyber <anuragrajput2603@gmail.com>
2025-08-04 22:52:20 +0530

    [Anurag] Add file reader with try-with-resources and IOException handling

---
+++ DAY4_Generics/src/com/gla/TryWithResource.java
@@ -0,0 +1,14 @@
+package com.gla;
+
+import java.io.*;
+
+public class TryWithResource{
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("info.txt"))) {
+            System.out.println("First line: " + br.readLine());
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
+
